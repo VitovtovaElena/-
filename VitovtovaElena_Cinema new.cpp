06ba2b8c7e345f34230fb29f4e86be37e588d8e8
VitovtovaElena <82289132+VitovtovaElena@users.noreply.github.com>
2021-04-21 08:47:22 +0400

    Add files via upload

--- VitovtovaElena_Cinema new.cpp
+++ VitovtovaElena_Cinema new.cpp
@@ -6,7 +6,8 @@ void DrowFonNight ();
 void DrowTextOut (int dvigx);
 void DrowFonSunrise ();
 void DrowFonStarts();
-void DrowTrain ();
+void DrowTrainNight (int grusx, int vagonx, int linex, int kolesox, int kolesoy, int vagonx_2);
+void DrowTrainSunrise (int grusx, int vagonx, int linex, int kolesox, int kolesoy, int vagonx_2);
 void DrowStar (int x, int y);
 void DrowRunHuman(int x, int y);
 void Human (int x, int y, COLORREF color);
@@ -14,20 +15,22 @@ void DrowCloud (int dvigX, int y, int radius);
 void DrowSun (int dop);
 
 int humanStep;
+int trainStep;
 
 int main ()
     {
     txCreateWindow (1024, 600);
     DrowFonNight ();
     DrowFonStarts();
-    DrowTrain ();
+    DrowTrainNight (10, 5,  200, 60, 560, 240);
     DrowTextOut (0);
 
 
     DrowFonSunrise ();
-    DrowTrain ();
+    DrowSun (5);
+    DrowCloud (125,50,20);
+    DrowTrainSunrise (10, 5,  200, 60, 560, 240);
     DrowRunHuman (500, 430);
-    DrowRunHuman (550, 450);
     DrowSun (5);
     DrowRunHuman (600, 430);
     DrowCloud (125,50,20);
@@ -46,7 +49,7 @@ int main ()
 
 void DrowFonSunrise ()
     {
-    txPlaySound ("atmosfera-prirodyi-na-rassvete", SND_ASYNC);
+    txPlaySound ("sounds/atmosfera-prirodyi-na-rassvete", SND_ASYNC);
     txSetFillColor (TX_LIGHTCYAN);
     txSetColor (TX_LIGHTCYAN);
     txRectangle (0, 0, 1024, 300);
@@ -64,33 +67,109 @@ void DrowFonSunrise ()
     txLine (0, 575, 1024, 575);
     }
 
-void DrowTrain ()
+void DrowTrainNight (int grusx, int vagonx, int linex, int kolesox, int kolesoy, int vagonx_2)
     {
-    txSetFillColor (TX_ORANGE);
-    txSetColor (TX_ORANGE);
-    txEllipse (10, 430, 195, 520);
+     txSetFillColor (TX_ORANGE);
+     txSetColor (TX_ORANGE);
+     txEllipse (grusx, 430, grusx+185, 520);
 
-    txSetFillColor (TX_GRAY);
-    txSetColor (TX_GRAY);
-    txRectangle (5, 460, 200, 550);
+     txSetFillColor (TX_GRAY);
+     txSetColor (TX_GRAY);
+     txRectangle (vagonx, 460, vagonx+195, 550);
 
-    txSetColor (TX_GRAY, 7);
-    txLine (200, 510, 240, 510);
+     txSetColor (TX_GRAY, 7);
+     txLine (linex, 510, linex+40, 510);
 
-    txSetFillColor (TX_NULL);
-    txSetColor ( TX_BLACK, 3);
-    txCircle (60, 560, 15);
-    txCircle (160, 560, 15);
-    txCircle (280, 560, 15);
-    txCircle (390, 560, 15);
+      txSetFillColor (TX_GRAY);
+      txSetColor (TX_GRAY);
+      txRectangle (vagonx_2, 380, vagonx_2+70, 550);
+      txRectangle (vagonx_2+70, 460, vagonx_2+200, 550);
 
-    txSetFillColor (TX_GRAY);
-    txSetColor (TX_GRAY);
-    txRectangle (240, 380, 330, 550);
-    txRectangle (330, 460, 440, 550);
+       txSetColor (TX_BLACK, 3);
+       txRectangle (vagonx_2+30, 400, vagonx_2+60, 470);
 
-    txSetColor (TX_BLACK, 3);
-    txRectangle (270, 400, 300, 470);
+       txSetFillColor (TX_NULL);
+       txSetColor ( TX_BLACK, 3);
+       txCircle (kolesox, kolesoy, 15);
+       txCircle (kolesox+100, kolesoy, 15);
+       txCircle (kolesox+220, kolesoy, 15);
+       txCircle (kolesox+330, kolesoy, 15);
+
+    }
+
+void DrowTrainSunrise (int grusx, int vagonx, int linex, int kolesox, int kolesoy, int vagonx_2)
+    {
+
+  trainStep=1;
+    while (trainStep<=35)
+       {
+       txSetColor (TX_MAGENTA, 5);
+       txLine (0, 560, 1024, 560);
+       txLine (0, 575, 1024, 575);
+
+       txSetFillColor (TX_ORANGE);
+       txSetColor (TX_ORANGE);
+       txEllipse (grusx, 430, grusx+185, 520);
+
+       txSetFillColor (TX_GRAY);
+       txSetColor (TX_GRAY);
+       txRectangle (vagonx, 460, vagonx+195, 550);
+
+       txSetColor (TX_GRAY, 7);
+       txLine (linex, 510, linex+40, 510);
+
+        txSetFillColor (TX_GRAY);
+        txSetColor (TX_GRAY);
+        txRectangle (vagonx_2, 380, vagonx_2+70, 550);
+        txRectangle (vagonx_2+70, 460, vagonx_2+200, 550);
+
+         txSetColor (TX_BLACK, 3);
+         txRectangle (vagonx_2+30, 400, vagonx_2+60, 470);
+
+         txSetFillColor (TX_NULL);
+         txSetColor ( TX_BLACK, 3);
+         txCircle (kolesox, kolesoy, 15);
+         txCircle (kolesox+100, kolesoy, 15);
+         txCircle (kolesox+220, kolesoy, 15);
+         txCircle (kolesox+330, kolesoy, 15);
+
+         txSleep (1000);
+
+         txSetFillColor (TX_GREEN);
+         txSetColor (TX_GREEN);
+         txEllipse (grusx, 430, grusx+185, 520);
+         txRectangle (vagonx, 460, vagonx+195, 550);
+         txRectangle (vagonx_2, 380, vagonx_2+70, 550);
+         txRectangle (vagonx_2+70, 460, vagonx_2+200, 550);
+         txRectangle (vagonx_2+30, 400, vagonx_2+60, 470);
+
+          txSetColor (TX_GREEN, 7);
+          txLine (linex, 510, linex+40, 510);
+
+         txSetFillColor (TX_NULL);
+         txSetColor ( TX_DARKGRAY, 3);
+         txCircle (kolesox, kolesoy, 15);
+         txCircle (kolesox+100, kolesoy, 15);
+         txCircle (kolesox+220, kolesoy, 15);
+         txCircle (kolesox+330, kolesoy, 15);
+
+        grusx+=10;
+        vagonx+=10;
+        linex+=10;
+        vagonx_2+=10;
+        kolesox+=10;
+
+        if (trainStep%2==0)
+          {
+          kolesoy=550;
+          }
+        else
+          {
+          kolesoy=560;
+           }
+         trainStep++;
+         txSleep (500);
+         }
     }
 
 void Human (int x, int y,COLORREF color)
@@ -150,7 +229,7 @@ void DrowTextOut (int dvigx)
     }
 void DrowFonNight ()
     {
-    txPlaySound ("nochnyie-sverchki.wav", SND_ASYNC);
+    txPlaySound ("sounds/nochnyie-sverchki.wav", SND_ASYNC);
     txSetFillColor (TX_BLUE);
     txSetColor (TX_BLUE);
     txRectangle (0, 0, 1024, 300);
